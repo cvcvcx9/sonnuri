7fc11e1328fb86dfb5061a26eabd8748427e2073
김상훈 <tpspfkdl@naver.com>
2024-11-12 13:42:23 +0900

    feat: 유사어 탐색

--- morpheme/app.py
+++ morpheme/app.py
@@ -1,3 +1,8 @@
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from dotenv import load_dotenv
+from openai import OpenAI
 import os
 import requests
 import xml.etree.ElementTree as ET
@@ -8,15 +13,38 @@ import torch
 from sklearn.metrics.pairwise import cosine_similarity
 from kobert_transformers import get_kobert_model, get_tokenizer
 
-# MongoDB 설정 (환경 변수 대신 직접 설정)
-MONGO_URI = "mongodb://localhost:27017/"
-MONGO_DB_NAME = "sonuri"
-MONGO_COLLECTION_NAME = "sign_word"
+# # MongoDB 설정 (환경 변수 대신 직접 설정)
+# MONGO_URI = "mongodb://localhost:27017/"
+# MONGO_DB_NAME = "sonuri"
+# MONGO_COLLECTION_NAME = "sign_word"
 
-# MongoDB 연결
-client = pymongo.MongoClient(MONGO_URI)
-db = client[MONGO_DB_NAME]
-collection = db[MONGO_COLLECTION_NAME]
+# # MongoDB 연결
+# client = pymongo.MongoClient(MONGO_URI)
+# db = client[MONGO_DB_NAME]
+# collection = db[MONGO_COLLECTION_NAME]
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# .env 파일 로드
+load_dotenv()
+
+# 환경 변수에서 MongoDB 사용자 이름과 비밀번호 불러오기
+mongo_username = os.getenv("MONGO_USERNAME")
+mongo_password = os.getenv("MONGO_PASSWORD")
+
+# MongoDB 클라이언트 설정 (예: 로컬호스트)
+mongo = pymongo.MongoClient(f"mongodb://{mongo_username}:{mongo_password}@k11a301.p.ssafy.io:8017/?authSource=admin")
+# mongo = pymongo.MongoClient("mongodb://localhost:27017/")
+db = mongo["sonnuri"]
+collection = db["sign_word"]
 
 # MongoDB에서 데이터 불러오기
 documents = collection.find()
@@ -25,7 +53,13 @@ documents = collection.find()
 word_data = pd.DataFrame(list(documents))
 
 # "단어"와 "설명" 열을 문자열로 변환한 후 결합하여 컨텍스트 문자열 생성
-word_data['context'] = word_data['단어'].astype(str) + ": " + word_data['설명'].astype(str)
+word_data['context'] = word_data['Word'].astype(str) + ": " + word_data['Description'].astype(str)
+
+# OPENAI 연결 설정
+openai = OpenAI(
+    organization="org-CdqlMsWjfTiylVQPL4TCGTy0",
+    api_key=os.getenv("OPENAI_API_KEY")
+)
 
 # KoBERT 모델 및 토크나이저 로드
 tokenizer = get_tokenizer()
@@ -33,6 +67,16 @@ model = get_kobert_model()
 
 # 임베딩 파일 경로
 embeddings_file = "kobert_embeddings.npy"
+
+class MatchedWord(BaseModel):
+    matched_word: str
+    matched_url: str
+    matched_description: str
+    definition: str
+
+class TextInput(BaseModel):
+    sentence: str
+    word: str
 
 # 임베딩 생성 함수
 def generate_embeddings(text_list):
@@ -86,13 +130,54 @@ def get_word_definition(query):
         return "정의 없음"
 
 # 유사한 단어와 URL을 찾는 함수
-def get_similar_word_url(query):
-    # 단어의 정의 가져오기
-    definition = get_word_definition(query)
-    if definition == "정의 없음":
-        print("단어의 정의를 찾을 수 없습니다.")
-        return None, None, None
+# def get_similar_word_url(query):
+#     # 단어의 정의 가져오기
+#     definition = get_word_definition(query)
+#     if definition == "정의 없음":
+#         print("단어의 정의를 찾을 수 없습니다.")
+#         return None, None, None
 
+#     # 정의를 임베딩 벡터로 변환
+#     inputs = tokenizer(definition, return_tensors='pt', truncation=True, padding=True, max_length=128)
+#     with torch.no_grad():
+#         outputs = model(**inputs)
+#         query_embedding = outputs.last_hidden_state[:, 0, :].numpy()
+
+#     # 코사인 유사도 계산
+#     similarities = cosine_similarity(query_embedding, embeddings)
+    
+#     # 가장 유사한 단어의 인덱스를 찾기
+#     most_similar_index = similarities.argmax()
+    
+#     # 유사한 단어, URL, 설명 반환
+#     matched_word = word_data.iloc[most_similar_index]['Word']
+#     matched_url = word_data.iloc[most_similar_index]['URL']
+#     matched_description = word_data.iloc[most_similar_index]['Description']
+    
+#     return matched_word, matched_url, matched_description, definition
+
+# 사용자로부터 단어 입력 받기
+# word = input("단어를 입력하세요: ")
+# matched_word, matched_url, matched_description = get_similar_word_url(word)
+
+# if matched_word and matched_url and matched_description:
+#     print(f"유사한 단어: {matched_word}\nURL: {matched_url}\n설명: {matched_description}")
+# else:
+#     print("관련된 정보를 찾을 수 없습니다.")
+    
+@app.post("/morpheme")
+async def morpheme(input_data: TextInput):
+    try:
+        matched_word, matched_url, matched_description, definition = get_similar_word_url(input_data)
+        return {"result": MatchedWord(matched_word=matched_word, matched_url=matched_url, matched_description=matched_description, definition=definition)}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error analyzing string: {str(e)}")
+    
+def get_similar_word_url(input_data: TextInput):
+    
+    # GPT로 문맥을 고려한 뜻 가져오기
+    definition = get_word_description_in_sentence(input_data.sentence, input_data.word)
+    
     # 정의를 임베딩 벡터로 변환
     inputs = tokenizer(definition, return_tensors='pt', truncation=True, padding=True, max_length=128)
     with torch.no_grad():
@@ -106,19 +191,18 @@ def get_similar_word_url(query):
     most_similar_index = similarities.argmax()
     
     # 유사한 단어, URL, 설명 반환
-    matched_word = word_data.iloc[most_similar_index]['단어']
-    matched_url = word_data.iloc[most_similar_index]['URL']
-    matched_description = word_data.iloc[most_similar_index]['설명']
+    matched_word = str(word_data.iloc[most_similar_index]['Word'])
+    matched_url = str(word_data.iloc[most_similar_index]['URL'])
+    matched_description = str(word_data.iloc[most_similar_index]['Description'])
     
-    return matched_word, matched_url, matched_description
+    return matched_word, matched_url, matched_description, definition
 
-# 사용자로부터 단어 입력 받기
-word = input("단어를 입력하세요: ")
-matched_word, matched_url, matched_description = get_similar_word_url(word)
-
-if matched_word and matched_url and matched_description:
-    print(f"유사한 단어: {matched_word}\nURL: {matched_url}\n설명: {matched_description}")
-else:
-    print("관련된 정보를 찾을 수 없습니다.")
-
-
+def get_word_description_in_sentence(sentence: str, word: str):
+    response = openai.chat.completions.create(
+    model="gpt-4o",
+    messages=[{"role": "system", "content": "너는 문맥을 파악해서 단어의 뜻을 알려주는 기계야."
+               "문장에서 단어의 뜻을 알려줘."
+               "응답은 단어의 뜻을 그대로 돌려주면 돼."},
+              {"role": "user", "content": f"문장: {sentence}, 단어: {word}"}]
+    )
+    return response.choices[0].message.content
