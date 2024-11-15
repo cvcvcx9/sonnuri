39f82230319bafd20925397cf190d98f5cf1487a
rlarbfla <rlarbfla1212@naver.com>
2024-11-15 19:31:10 +0900

    video

--- sonnuri/src/FuncThird.js
+++ sonnuri/src/FuncThird.js
@@ -2,6 +2,8 @@ import func3 from "./image/func3.png";
 import InfoSection from "./InfoSection";
 import VideoSection from "./VideoSection";
 import Dot from "./Dot";
+import positionsign from "./image/위치고정.png"
+
 
 function FuncThird() {
   return ( 
@@ -12,6 +14,8 @@ function FuncThird() {
         title="위치 고정" 
         description="필요한 위치에 영상 창을 고정할 수 있습니다." 
         image={func3} 
+        titleImage={positionsign}
+        titleImageWidth="300px" 
       />
 
       {/* 오른쪽 콘텐츠: 영상 */}
--- sonnuri/src/InfoSection.js
+++ sonnuri/src/InfoSection.js
@@ -1,19 +1,30 @@
 // InfoSection.js
-import React from 'react';
+import React from "react";
 
-function InfoSection({ number, title, description, image }) {
+function InfoSection({ number, title, description, image, titleImage, titleImageWidth  }) {
   return (
     <div className="flex flex-col items-start space-y-4 w-1/2 pt-20 pl-60 pb-8">
       <div className="flex items-center space-x-2">
         <span className="flex items-center justify-center w-8 h-8 bg-blue-600 text-white rounded-full text-lg font-semibold">
           {number}
         </span>
-        <h2 className="text-4xl font-semibold pl-2">{title}</h2>
+        {/* <h2 className="text-4xl font-semibold pl-2">{title}</h2> */}
+
+        <img
+              src={titleImage}
+              alt="Title 이미지"
+              className="h-auto"
+              style={{ width: titleImageWidth }}
+            />
       </div>
       <div className="text-gray-700 pt-4 text-2xl">
         <p>{description}</p>
       </div>
-      <img src={image} alt={`${title} 이미지`} className="w-auto h-80 pt-4 ml-20" />
+      <img
+        src={image}
+        alt={`${title} 이미지`}
+        className="w-auto h-80 pt-4 ml-20"
+      />
     </div>
   );
 }
