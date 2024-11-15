3766992632ac46d4666dbdd429fbddd20236a5b3
cvcvcx9 <cvcvcx9@gmail.com>
2024-11-15 11:57:10 +0900

    Chore: 비디오쪽 css로직변경

--- extension/sonnuri/pages/side-panel/src/SidePanel.tsx
+++ extension/sonnuri/pages/side-panel/src/SidePanel.tsx
@@ -1,9 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { Button, Card, CardContent, Tabs, TabsContent, TabsList, TabsTrigger } from '@extension/ui/lib/components/ui';
 import React, { useEffect, useState } from 'react';
 import ReactPlayer from 'react-player';
-import { Button, Card, CardContent } from '@extension/ui/lib/components/ui';
 import SkeletonLoader from './components/SkeletonLoader';
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@extension/ui/lib/components/ui';
 
 const SidePanel: React.FC = () => {
   // 문장 리스트
@@ -107,89 +106,98 @@ const SidePanel: React.FC = () => {
         <CardContent className="p-4 min-h-[500px]">
           <Tabs defaultValue="sentence" className="w-full">
             <TabsList className="flex justify-center">
-          <TabsTrigger value="sentence" className="text-sm w-full">
-            단어별 보기
-          </TabsTrigger>
-          <TabsTrigger value="interpolate" className="text-sm w-full" disabled={interpolateLoading}>
-            수어
-          </TabsTrigger>
-        </TabsList>
-        <TabsContent value="sentence">
-          <div className="grid md:grid-cols-2 gap-8">
-            {interpolateLoading && <div className="text-center text-gray-500">이어보기 비디오 생성중...</div>}
-            <div className="flex flex-wrap gap-2">
-              {playlistInfo && playlistInfo.length > 0 ? (
-                playlistInfo.map((item, index) => (
-                  <Button
-                    
-                    key={index}
-                    onClick={() => handleButtonClick(item.isFirstIdx)}
-                    className="text-sm bg-[#C1C1C1] px-2 py-1 text-white rounded-full">
-                    {item.word}
-                  </Button>
-                ))
-              ) : (
-                <div className="text-center text-gray-500">저장된 문장이 없습니다.</div>
-              )}
-            </div>
-            <div className="space-y-4">
-              <div className="player-wrapper">
-                {isLoading ? (
-                  <SkeletonLoader />
-                ) : playlist.length > 0 ? (
-                  <>
-                    <ReactPlayer
-                      url={playlist[currentVideoIndex]}
-                      // url={"https://sonnuri.s3.ap-northeast-2.amazonaws.com/sentence/processed_52a78f3c57b475c024e9a8b7329cbf9c.mp4"}
-                      controls={false}
-                      playing
-                      playbackRate={playerSpeed}
-                      width="100%"
-                      height="200px"
-                      onEnded={handleVideoEnd}
-                      style={{ backgroundColor: '#000000' }}
-                      />
-                    <div className="mt-2">
-                      재생 중: {currentVideoIndex + 1} / {playlist.length}
-                    </div>
-                  </>
-                ) : (
-                  <div className="bg-black w-full h-[200px] rounded-lg flex items-center justify-center">
-                    <ReactPlayer width="100%" height="200px" style={{ backgroundColor: '#000000' }} />
+              <TabsTrigger value="sentence" className="text-sm w-full">
+                <img className="w-8 h-8" src="./word_sign_language.png" alt="word" />
+              </TabsTrigger>
+              <TabsTrigger value="interpolate" className="text-sm w-full" disabled={interpolateLoading}>
+                <img className="w-8 h-8" src="./sentence_sign_language.png" alt="word" />
+              </TabsTrigger>
+            </TabsList>
+            <TabsContent value="sentence">
+              <div className="grid md:grid-cols-2 gap-8">
+                {interpolateLoading && <div className="text-center text-gray-500">이어보기 비디오 생성중...</div>}
+                <div className="flex flex-wrap gap-2 mt-4">
+                  {playlistInfo && playlistInfo.length > 0 ? (
+                    playlistInfo.map((item, index) => (
+                      <Button
+                        key={index}
+                        onClick={() => handleButtonClick(item.isFirstIdx)}
+                        className="text-sm bg-[#C1C1C1] px-2 py-1 text-white rounded-full">
+                        {item.word}
+                      </Button>
+                    ))
+                  ) : (
+                    <div className="text-center text-gray-500">저장된 문장이 없습니다.</div>
+                  )}
+                </div>
+                <div className="space-y-4">
+                  <div className="player-wrapper">
+                    {isLoading ? (
+                      <SkeletonLoader />
+                    ) : playlist.length > 0 ? (
+                      <>
+                        <ReactPlayer
+                          url={playlist[currentVideoIndex]}
+                          // url={"https://sonnuri.s3.ap-northeast-2.amazonaws.com/sentence/processed_52a78f3c57b475c024e9a8b7329cbf9c.mp4"}
+                          controls={false}
+                          playing
+                          playbackRate={playerSpeed}
+                          width="100%"
+                          height="200px"
+                          onEnded={handleVideoEnd}
+                          style={{ backgroundColor: '#000000' }}
+                        />
+                        <div className="mt-2">
+                          재생 중: {currentVideoIndex + 1} / {playlist.length}
+                        </div>
+                      </>
+                    ) : (
+                      <div className="bg-black w-full h-[200px] rounded-lg flex items-center justify-center">
+                        <ReactPlayer width="100%" height="200px" style={{ backgroundColor: '#000000' }} />
+                      </div>
+                    )}
                   </div>
-                )}
+                </div>
               </div>
-            </div>
-          </div>
-        </TabsContent>
-        <TabsContent value="interpolate">
-          <h2 className="text-md font-bold">
-            {originalText}
-          </h2>
-          <ReactPlayer
-            url={interpolatedUrl}
-            controls={false}
-            playing
-            playbackRate={playerSpeed}
-            width="100%"
-            height="200px"
-            onEnded={handleVideoEnd}
-            style={{ backgroundColor: '#000000' }}
-            />
-          <div className="mt-2"></div>
-        </TabsContent>
-      </Tabs>
-      <div className="flex justify-center pt-6 items-center">
-        <button onClick={() => setPlayerSpeed(playerSpeed - 0.1)} disabled={playerSpeed <= 0.5}>
-          <img className="w-8 h-8" src="./slower.png" alt="minus" />
-        </button>
-        <button onClick={() => setPlayerSpeed(playerSpeed + 0.1)} disabled={playerSpeed >= 2.0}>
-          <img className="w-8 h-8" src="./faster.png" alt="plus" />
-        </button>
-      </div>
-      </CardContent>
+            </TabsContent>
+            <TabsContent value="interpolate">
+              <h2 className="text-lg font-bold">
+                원문
+              </h2>
+              <h3 className="text-sm text-center mt-2">{originalText}</h3>
+              <h2 className="text-lg font-bold mt-4">
+                번역된 수어 비디오
+              </h2>
+              <Card className="w-full">
+                <CardContent className="p-4">
+                  <ReactPlayer
+                    url={interpolatedUrl}
+                    controls={false}
+                    playing
+                    playbackRate={playerSpeed}
+                    width="100%"
+                    height="200px"
+                    onEnded={handleVideoEnd}
+                    style={{ backgroundColor: '#000000' }}
+                  />
+                  <div className="mt-2">
+                    재생 속도 : {playerSpeed.toFixed(1)}x
+                  </div>
+                  <div className="flex justify-center pt-6 items-center gap-4">
+                    <button onClick={() => setPlayerSpeed(playerSpeed - 0.1)} disabled={playerSpeed <= 0.5}>
+                      <img className="w-8 h-8" src="./slower.png" alt="minus" />
+                    </button>
+                    <button onClick={() => setPlayerSpeed(playerSpeed + 0.1)} disabled={playerSpeed >= 3.0}>
+                      <img className="w-8 h-8" src="./faster.png" alt="plus" />
+                    </button>
+                  </div>
+                </CardContent>
+              </Card>
+            </TabsContent>
+          </Tabs>
+        </CardContent>
       </Card>
-      
+
     </div>
   );
 };
